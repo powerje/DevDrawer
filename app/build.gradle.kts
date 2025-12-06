e9ddb71354ab8dcfaf44c2091e87afea976591b1
James Power <jep@lepoer.us>
2025-12-06 06:24:11 -0500

    fix: Lower minSdk to 26 to fix installation issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,10 +16,10 @@ android {
 
     defaultConfig {
         applicationId = "com.powerje.devdrawer"
-        minSdk = 35
+        minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
