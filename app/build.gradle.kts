74af1c7c052c74924395508d8fc50b9b413fc30f
James Power <jep@lepoer.us>
2025-12-06 07:15:59 -0500

    Bump version code to 4.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.powerje.devdrawer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
